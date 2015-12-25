bad038a5b0c3a6369f9d2476e4a107059554cfd3
Robert Foss <dev@robertfoss.se>
2015-12-25 12:05:08 -0500

    Added get_ip sample.

---
+++ get_ip.lua
@@ -0,0 +1,15 @@
+-- Set up WiFI
+wifi.setmode(wifi.STATION)
+wifi.sta.config("", "")
+
+-- Tell us when we've gotten an IP
+tmr.alarm(0, 500, 1, function()
+   if wifi.sta.getip()==nil
+    then
+      print("Connecting to AP...")
+   else
+      print("Connected as: " .. wifi.sta.getip())
+      tmr.stop(0)
+   end
+end)
+
